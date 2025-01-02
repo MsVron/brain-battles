1726a1cc210471f508568e88ac998e07b8c8da85
MsVron <ayaberrou01@gmail.com>
2025-01-02 16:22:05 +0100

    duplicate fix pt.2

--- src/main/java/com/quiz/database/dao/UserScoreDao.java
+++ src/main/java/com/quiz/database/dao/UserScoreDao.java
@@ -10,28 +10,28 @@ import java.util.List;
 public class UserScoreDao {
 
     // Méthode pour récupérer les scores depuis la base de données
-    public static List<PlayerScore> getLeaderboard() {
-        List<PlayerScore> leaderboard = new ArrayList<>();
-        String query = "SELECT u.username, us.score FROM userScores us " +
-                       "JOIN users u ON us.userId = u.userId " +
-                       "ORDER BY us.score DESC LIMIT 100"; // Récupère les 100 meilleurs scores
+	public static List<PlayerScore> getLeaderboard() {
+	    List<PlayerScore> leaderboard = new ArrayList<>();
+	    String query = "SELECT u.username, MAX(us.score) as score FROM userScores us " +
+	                   "JOIN users u ON us.userId = u.userId " +
+	                   "GROUP BY u.userId " +
+	                   "ORDER BY MAX(us.score) DESC LIMIT 100";
 
-        try (Connection conn = DatabaseConnection.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(query);
-             ResultSet rs = stmt.executeQuery()) {
+	    try (Connection conn = DatabaseConnection.getConnection();
+	         PreparedStatement stmt = conn.prepareStatement(query);
+	         ResultSet rs = stmt.executeQuery()) {
 
-            while (rs.next()) {
-                String username = rs.getString("username");
-                int score = rs.getInt("score");
-                // Ajout d'un nouveau PlayerScore à la liste
-                leaderboard.add(new PlayerScore(username, score));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+	        while (rs.next()) {
+	            String username = rs.getString("username");
+	            int score = rs.getInt("score");
+	            leaderboard.add(new PlayerScore(username, score));
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
 
-        return leaderboard;
-    }
+	    return leaderboard;
+	}
     
     public static boolean saveScore(int userId, int quizId, int score) {
         Connection conn = null;
